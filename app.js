88ee351eb20ffbed1d9d0328f382a421ad6dfb34
lucius-César <luciendelgrange5@gmail.com>
2024-03-22 04:04:48 +0100

    cors default parameters

--- app.js
+++ app.js
@@ -27,12 +27,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(
-  cors({
-    origin: "http://localhost:3001",
-    credentials: true,
-  })
-);
+app.use(cors());
+
+/* please use this cors parameters in developement
+cors({
+  origin: "http://localhost:3001",
+  credentials: true,
+})
+*/
 app.use(express.json());
 app.use(passport.initialize());
 app.use(cookieParser());
